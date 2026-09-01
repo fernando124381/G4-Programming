8e831ff4821061bbd3fa04e0ce18a6ddeb4ecdd9
Fernando124381 <fernando124381@mcpsva.org>
2026-09-01 15:19:08 -0400

    HI

--- interview.py
+++ interview.py
@@ -1,6 +1,24 @@
+"""
+Filename: interview.py
+Author: <Lastname, Firstname>
+Created: <MM/DD/YYYY>
+Instructor: Burgess
+"""
 
 
+age1 = input("How old are you")
+age2 = input("How old are you again?")
 
-print("Hello!")
+print(f"You entered: {age1} you also entered: {age2}")
+print(age2)
 
 
+#print("Hello!",20+30,"Goodbye!"
+#print("This is on a new line")
+
+
+print=("how are you")
+
+age = input("inputI'm good thanks for ask\nHow Grade are you?")
+
+print(f"if you have {age} your in high school")
