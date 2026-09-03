28a116da9ae33bf1af4ab59701d8a6d0b573cb68
Fernando124381 <fernando124381@mcpsva.org>
2026-09-03 15:13:36 -0400

    HI new

--- interview.py
+++ interview.py
@@ -5,20 +5,28 @@ Created: <MM/DD/YYYY>
 Instructor: Burgess
 """
 
+input ("name")
+
+input ("student ID")
 
 age1 = input("How old are you")
 age2 = input("How old are you again?")
 
 print(f"You entered: {age1} you also entered: {age2}")
-print(age2)
+
+input ("What is the name of the room we are in?")
+
+input ("What year of high school are you in?")
+
+input ("You have phone?")
+
+input ("How much do you know about math, on a scale of 1 to 10?")
+
+
+
 
 
 #print("Hello!",20+30,"Goodbye!"
 #print("This is on a new line")
 
 
-print=("how are you")
-
-age = input("inputI'm good thanks for ask\nHow Grade are you?")
-
-print(f"if you have {age} your in high school")
