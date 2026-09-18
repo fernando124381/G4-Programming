14450dc7affc16c5e974f8b6999e3a39e1ccf240
Fernando124381 <Fernando124381@mcpsva.org>
2026-09-18 15:18:54 -0400

    New python

---
+++ MadLib.py
@@ -0,0 +1,53 @@
+Nombre de una persona: ___________________________
+Adjetivo: ___________________________
+Lugar: ___________________________
+Objeto (plural): ___________________________
+Animal: ___________________________
+Verbo en infinitivo: ___________________________
+Líquido: ___________________________
+Parte del cuerpo (plural): ___________________________
+Sustantivo: ___________________________
+Profesión: ___________________________
+Color: ___________________________
+Comida (plural): ___________________________
+Verbo en pasado: ___________________________
+Emoción: ___________________________
+Adjetivo: ___________________________
+Prenda de vestir: ___________________________
+Exclamación o grito: ___________________________
+Numero: ___________________________
+Verbo en infinitivo: ___________________________
+Sustantivo: ___________________________
+
+Nombredeunapersona =input ("Fernado")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+=input ("")
+
+
+
+
+
+
+
+
+
+
+
