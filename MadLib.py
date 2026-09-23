6e2c58bb6e77985c43eb17522548fe251038d4ba
Fernando124381 <Fernando124381@mcpsva.org>
2026-09-23 15:15:55 -0400

    New python.

--- MadLib.py
+++ MadLib.py
@@ -1,50 +1,36 @@
-Nombre de una persona: ___________________________
-Adjetivo: ___________________________
-Lugar: ___________________________
-Objeto (plural): ___________________________
-Animal: ___________________________
-Verbo en infinitivo: ___________________________
-Líquido: ___________________________
-Parte del cuerpo (plural): ___________________________
-Sustantivo: ___________________________
-Profesión: ___________________________
-Color: ___________________________
-Comida (plural): ___________________________
-Verbo en pasado: ___________________________
-Emoción: ___________________________
-Adjetivo: ___________________________
-Prenda de vestir: ___________________________
-Exclamación o grito: ___________________________
-Numero: ___________________________
-Verbo en infinitivo: ___________________________
-Sustantivo: ___________________________
+Person_name=input ("")
+Adjective=input ("")
+Place=input ("")
+Object_plural=input ("")
+Animal=input ("")
+Verb_infinitive1=input ("")
+Liquid=input ("")
+Body_part_plural=input ("")
+Noun1=input ("")
+Profession=input ("")
+Color=input ("")
+Food_plural=input ("")
+Verb_past_tense=input ("")
+Emotion=input ("")
+Adjective_=input ("")
+Article_of_clothing=input ("")
+Exclamation_or_shout=input ("")
+Number=input ("")
+Verb_infinitive2=input ("")
+Noun2=input ("")
+print (f"Yesterday morning,{Person_name} woke up with a very {Adjective} feeling.")
+print (f"He looked out the window and realized he was no longer at home, but in the middle of {Place}.")
+print (f"On the floor, there were no rugs, but a pile of shiny {Object_plural},")
+print (f"And to his surprise, a giant {Animal} was trying to {Verb_infinitive1} in the corner of the room.")
 
-=input ("Fernado")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
-=input ("")
+print (f"To clear his mind, he decided to drink a large glass of ice-cold {Liquid},")
+print (f"But as he brought it to his lips, he felt his {Body_part_plural} start to blink uncontrollably.")
+print (f"Suddenly, a secret {Noun1} opened in the wall, and a mysterious {Profession} appeared,")
+print (f"Wearing a suit of a brilliant {Color} color.")
+print (f"This strange character was carrying a tray full of magical {Food_plural} and {Verb_past_tense} hard before speaking.")
 
-
-
-
-
+print (f"With a deep {} on his face, the visitor told him, You have a very {} mission,")
+print (f"You must put on this sacred {} right now. Unable to contain himself, our hero let out a loud {}.")
 
 
 
